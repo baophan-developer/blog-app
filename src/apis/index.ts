6337042810e73787d65fcc5ae0ab2b4cc8dda886
baoph <baoph@unit.com.vn>
2025-04-26 11:41:52 +0700

    update apis

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,4 +1,4 @@
-import {IPost} from "../interfaces";
+import {IComment, IPost} from "../interfaces";
 import {getDataLocalStore, setDataLocalStore} from "../utils";
 
 export async function login(username: string, password: string) {
@@ -22,10 +22,47 @@ export async function register(username: string, password: string) {
 	}
 }
 
-export async function createPost(post: IPost) {
+export async function createPost(post: Omit<IPost, "id">) {
 	try {
-		setDataLocalStore("posts", post);
+		const result = getDataLocalStore("posts");
+		const currentIdPost = [...result][result.length - 1].id;
+		setDataLocalStore("posts", [...result, {id: currentIdPost + 1, ...post}]);
 		return "Create post success!";
+	} catch (error: any) {
+		return error?.message || "Error";
+	}
+}
+
+export async function createComment(postId: number, comment: Omit<IComment, "id">) {
+	try {
+		const result = getDataLocalStore("posts");
+		if (!result) throw new Error("Cannot comment!");
+		const posts = [...result].filter((p) => p.id !== postId);
+		const post = [...result].find((p) => p.id === postId);
+		const currentIdComment = [...result][result.length - 1].id || 0;
+		setDataLocalStore("posts", [
+			...posts,
+			{
+				...post,
+				comments: [...post.comments, {id: currentIdComment + 1, ...comment}],
+			},
+		]);
+		return "Add comment success";
+	} catch (error: any) {
+		return error?.message || "Error";
+	}
+}
+
+export async function updateComment(
+	postId: number,
+	commentId: number,
+	comment: Omit<IComment, "id">,
+) {
+	try {
+		const result = getDataLocalStore("posts");
+		if (!result) throw new Error("Cannot update comment!");
+		const post = [...result].find((p) => p.id === postId);
+		if (!post) throw new Error("Cannot find post!");
 	} catch (error: any) {
 		return error?.message || "Error";
 	}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -5,17 +5,21 @@ export interface IUser {
 }
 
 export interface IComment {
+	id: number;
 	username: string;
 	createdDate: string;
 	content: string;
+	edit: boolean;
 }
 
 export interface IPost {
+	id: number;
 	title: string;
 	content: string;
 	createdDate: string;
 	updatedDate: string;
 	comments: Array<IComment>;
+	edit: boolean;
 }
 
 export interface IStore {
